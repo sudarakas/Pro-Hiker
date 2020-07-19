5d43021c91b4332a5e31f26969896534c4bfb787
sudarakas <sudharakafb@gmail.com>
2020-07-19 15:08:53 +0530

    added mongoose and connected the db

--- server.js
+++ server.js
@@ -1,6 +1,48 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
 const dotenv = require(`dotenv`);
+const mongoose = require(`mongoose`);
 dotenv.config({ path: `./config.env` });
 const app = require(`./app`);
+
+//database connection string
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+//connect the database
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB Connected');
+  });
+
+//hikes scheme
+const hikesScheme = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A hike must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 0.0,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A hike must have a price'],
+  },
+});
+
+//create the model with scheme
+const Hike = mongoose.model('Hike', hikesScheme);
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
