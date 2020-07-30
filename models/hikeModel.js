c667879fd2e2d3278fb962dab8e9c2f38a578725
sudarakas <sudharakafb@gmail.com>
2020-07-30 12:19:35 +0530

    added virtual properties example

--- models/hikeModel.js
+++ models/hikeModel.js
@@ -1,68 +1,79 @@
 const mongoose = require(`mongoose`);
 
 //hikes scheme
-const hikesScheme = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A hike must have a name'],
-    unique: true,
-    trim: true,
+const hikesScheme = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A hike must have a name'],
+      unique: true,
+      trim: true,
+    },
+    ratingAverage: {
+      type: Number,
+      default: 0.0,
+    },
+    ratingQty: {
+      type: Number,
+      default: 0,
+    },
+    duration: {
+      type: Number,
+      required: [true, 'A hike must have a duration'],
+    },
+    minGroupSize: {
+      type: Number,
+      required: [true, 'A hike must have a minimum group size'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A hike must have a maximum group size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A hike must have a difficulty'],
+    },
+    price: {
+      type: Number,
+      required: [true, 'A hike must have a price'],
+    },
+    priceDiscount: {
+      type: Number,
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: [true, 'A hike must have a description'],
+    },
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A hike must have a summary'],
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A hike must have a cover image'],
+    },
+    images: {
+      type: [String],
+      required: [true, 'A hike must have a cover image'],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    startDates: [Date],
   },
-  ratingAverage: {
-    type: Number,
-    default: 0.0,
-  },
-  ratingQty: {
-    type: Number,
-    default: 0,
-  },
-  duration: {
-    type: Number,
-    required: [true, 'A hike must have a duration'],
-  },
-  minGroupSize: {
-    type: Number,
-    required: [true, 'A hike must have a minimum group size'],
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A hike must have a maximum group size'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A hike must have a difficulty'],
-  },
-  price: {
-    type: Number,
-    required: [true, 'A hike must have a price'],
-  },
-  priceDiscount: {
-    type: Number,
-  },
-  description: {
-    type: String,
-    trim: true,
-    required: [true, 'A hike must have a description'],
-  },
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, 'A hike must have a summary'],
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A hike must have a cover image'],
-  },
-  images: {
-    type: [String],
-    required: [true, 'A hike must have a cover image'],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false,
-  },
-  startDates: [Date],
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+//virtual properties
+hikesScheme.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
 });
 
 //create the model with scheme
