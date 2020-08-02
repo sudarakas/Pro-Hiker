70a92a83f76f62e1c3d4240e3e9b4e635bea2634
sudarakas <sudharakafb@gmail.com>
2020-08-02 20:50:39 +0530

    added errorController and AppError class

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 /* eslint-disable no-console */
 const express = require('express');
 const morgan = require('morgan');
+
+const AppError = require('./utils/appError');
+
+const errorHandler = require('./controllers/userController');
 const hikeRouter = require('./routes/hikeRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -24,20 +28,9 @@ app.use('/api/v1/hikes', hikeRouter);
 
 //for all undefined routes
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on the server`,
-  });
+  next(new AppError(`Can't find ${req.originalUrl} on the server`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+//app.use();
 
 module.exports = app;
