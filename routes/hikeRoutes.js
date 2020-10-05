37fe8ce748e5dd93d8137866875448ae09d113d1
sudarakas <sudharakafb@gmail.com>
2020-10-05 19:56:26 +0530

    add nested routes with express for review and  hike

--- routes/hikeRoutes.js
+++ routes/hikeRoutes.js
@@ -3,10 +3,14 @@ const express = require('express');
 const router = express.Router();
 const hikeController = require('../controllers/hikeController');
 const authController = require('../controllers/authController');
-const reviewController = require('../controllers/reviewController');
+
+const reviewRouter = require('./reviewRoutes');
 
 //can use this type middleware to validate id
 // router.param('id', checkHikeId);
+
+//Nested routes - Create new review
+router.use('/:hikeId/reviews', reviewRouter);
 
 router
   .route('/top-5-cheap')
@@ -29,14 +33,6 @@ router
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
     hikeController.deleteHike
-  );
-
-router
-  .route('/:hikeId/reviews')
-  .post(
-    authController.protect,
-    authController.restrictTo('user'),
-    reviewController.createReview
   );
 
 module.exports = router;
