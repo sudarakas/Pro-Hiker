cff40b3fff3f218820a651aa4cdf3636c20f1791
sudarakas <sudharakafb@gmail.com>
2020-10-30 21:30:23 +0530

    add func to get distance for a hike from geo location

--- controllers/hikeController.js
+++ controllers/hikeController.js
@@ -121,3 +121,39 @@ exports.getHikeWithinRange = catchAsync(async (req, res, next) => {
     },
   });
 });
+
+exports.getDistances = catchAsync(async (req, res, next) => {
+  const { latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+
+  const multilper = unit === 'mi' ? 0.000621371 : 0.001;
+
+  if (!lat || !lng)
+    next(new AppError('lat and lng placed on wrong order', 400));
+
+  const distances = await Hike.aggregate([
+    {
+      $geoNear: {
+        near: {
+          type: 'Point',
+          coordinates: [lng * 1, lat * 1],
+        },
+        distanceField: 'distance',
+        distanceMultiplier: multilper,
+      },
+    },
+    {
+      $project: {
+        distance: 1,
+        name: 1,
+      },
+    },
+  ]);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      data: distances,
+    },
+  });
+});
